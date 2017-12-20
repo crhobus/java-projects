acbd01a8fb17c0a9025ddf33cdf555c6165161ac
crhobus <caiohobus@gmail.com>
2017-12-20 17:09:00 -0200

    init

---
+++ netbeans-projects/OperadoresIncrementoDecremento/src/Operadores/OperadoresIncrementoDecremento.java
@@ -0,0 +1,33 @@
+package Operadores;
+
+
+public class OperadoresIncrementoDecremento {
+
+    public static void main(String[] args) {
+        int c, x;
+        c = 5;
+        x = 105;
+
+        System.out.println("OPERADOR DE INCREMENTO");
+        System.out.println(c);
+        System.out.println(c++);
+        System.out.println(c);
+        System.out.println();
+
+        System.out.println(c);
+        System.out.println(++c);
+        System.out.println(c);
+        System.out.println();
+
+        System.out.println("OPERADORES DE DECREMENTO");
+        System.out.println(x);
+        System.out.println(x--);
+        System.out.println(x);
+        System.out.println();
+
+        System.out.println(x);
+        System.out.println(--x);
+        System.out.println(x);
+        System.out.println();
+    }
+}
